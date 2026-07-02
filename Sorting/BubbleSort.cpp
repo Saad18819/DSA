602b7ff372bf8ad7ef7a29c8dc50310661b3f87d
Saad18819 <eucalyptuszenthura1234@gmail.com>
2026-07-02 13:56:05 +0530

    dsa

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -10,12 +10,18 @@ void PrintArray(int *arr,int n){
 
 void bubbleSort(int *arr , int n){
     for(int i=0;i<n-1;i++){
+        bool isSorted = false;
         for(int j=0;j<n-i-1;j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isSorted = true;
             }
         }
-    }   
+        // !isSorted means false
+        if(!isSorted){
+            break;
+        }
+    }
     PrintArray(arr , n);
 }
 
